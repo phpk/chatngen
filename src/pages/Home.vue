053909198be408832802043d68e628c0ee0bf8ac
rtbz <ryanbell@gmail.com>
2018-11-17 10:24:52 -0800

    Added QR Code + Share Links + Embed

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -28,6 +28,84 @@
           </v-btn>
         </v-layout>
       </v-layout>
+      <v-layout justify-center>
+        <v-btn
+          @click="showLink = !showLink"
+          class="transparent"
+          color="white"
+          flat
+        >
+          {{ showLink ? 'Hide' : 'Share' }}
+          Link <v-icon class="ml-1">link</v-icon>
+        </v-btn>
+        <v-btn
+          @click="showEmbedGen = !showEmbedGen"
+          class="transparent"
+          color="white"
+          flat
+        >
+          {{ showEmbedGen ? 'Hide' : 'Get' }}
+          Embed <v-icon class="ml-1">code</v-icon>
+        </v-btn>
+      </v-layout>
+      <v-layout
+        style="border: 2px solid #fff; border-radius: 4px"
+        class="pa-4 mt-3"
+        v-if="showLink"
+        column
+      >
+        <div class="headline white--text mb-2">Share link</div>
+        <v-text-field
+          label="URL"
+          :value="roomLink()"
+          color="white"
+          @focus="$event.target.setSelectionRange(0, $event.target.value.length)"
+          @click="$event.target.setSelectionRange(0, $event.target.value.length)"
+          readonly
+          hide-details
+          dark
+        />
+        <div class="mt-2" id="shareQr"></div>
+      </v-layout>
+      <v-layout
+        style="border: 2px solid #fff; border-radius: 4px"
+        class="pa-4 mt-3"
+        v-if="showEmbedGen"
+        column
+      >
+        <div class="headline white--text mb-2">Generate An Embed Code</div>
+        <v-layout>
+          <v-text-field
+            label="Width"
+            v-model="embed.width"
+            color="white"
+            class="mr-2"
+            hide-details
+            dark
+          />
+          <v-text-field
+            label="Height"
+            v-model="embed.height"
+            class="ml-2"
+            color="white"
+            hide-details
+            dark
+          />
+        </v-layout>
+        <v-textarea
+          label="Embed Code"
+          prepend-inner-icon="code"
+          class="mt-3 white--text mono"
+          color="white"
+          @focus="$event.target.setSelectionRange(0, $event.target.value.length)"
+          @click="$event.target.setSelectionRange(0, $event.target.value.length)"
+          :value="genEmbed()"
+          readonly
+          dark
+          box
+          hide-details
+        />
+      </v-layout>
     </div>
   </v-layout>
 </template>
@@ -36,23 +114,69 @@
 export default {
   data() {
     return {
+      embed: {
+        defaults: {
+          width: 720,
+          height: 460
+        },
+        width: 720,
+        height: 460
+      },
+      qrCode: null,
+      showLink: false,
+      showEmbedGen: false,
       room: null
     }
   },
   methods: {
-    joinRoom() {
-      const formattedRoom = this.room || 'lobby'
+    createQr() {
+      this.qrCode = new window.QRCode('shareQr', {
+        text: this.roomLink(),
+        width: 296,
+        height: 296,
+        colorDark: '#ffffff',
+        colorLight: 'transparent',
+        correctLevel: window.QRCode.CorrectLevel.L
+      })
+    },
+    updateQr() {
+      this.qrCode.clear()
+      this.qrCode.makeCode(this.roomLink())
+    },
+    redrawQr() {
+      this.qrCode ? this.updateQr() : this.createQr()
+    },
+    formatRoom(room) {
+      return (room || 'lobby')
         .replace(/^https?:\/\//, '')
         .replace(/[\W_]+/g, ' ')
         .replace(/ /g, '-')
         .replace(/-$/, '')
         .toLowerCase()
-      this.$router.push(formattedRoom)
+    },
+    roomLink() {
+      return `https://chatngen.com/#/${this.formatRoom(this.room)}`
+    },
+    genEmbed() {
+      const w = parseInt(this.embed.width, 10) || this.embed.defaults.width
+      const h = parseInt(this.embed.height, 10) || this.embed.defaults.height
+      return `<iframe width="${w}" height="${h}" src="${this.roomLink()}" frameborder="0"></iframe>`
+    },
+    joinRoom() {
+      this.$router.push(this.formatRoom(this.room))
       this.room = null
     }
   },
   mounted() {
     this.$refs.roomInput.focus()
+  },
+  watch: {
+    room() {
+      if (this.showLink) this.redrawQr()
+    },
+    showLink(val) {
+      if (val) this.$nextTick(() => this.redrawQr())
+    }
   }
 }
 </script>
